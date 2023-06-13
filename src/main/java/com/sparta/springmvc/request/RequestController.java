f9a49a25623b5080e8b6f6dd248155798035d851
azuressu <martina0130@naver.com>
2023-06-13 10:35:47 +0900

    HTTP 데이터를 객체로 처리하는 방법

--- src/main/java/com/sparta/springmvc/request/RequestController.java
+++ src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -39,9 +39,55 @@ public class RequestController {
     // name=Robbie&age=95
     @PostMapping("/form/param")
     @ResponseBody
-    public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
+    public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) { // @RequestParam도 생략 가능
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
     }
+
+    // Request Sample
+    // POST http://localhost:8080/hello/request/form/model
+    // Header
+    // Content type: application/x-www-form-urlencoded
+    // Body
+    // name=Robbie&age=95
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestBodyForm(@ModelAttribute Star star) {
+        // @ModelAttrubute annotation을 사용하면 body 부분에 들어온 query string 방식의 데이터를
+        // 객체에 mapping 해서 가지고 올 수 있음
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+        // 이름 자체를 클라이언트에서 보내주는 이름과 동일하게 맞춰주어야 함. (name이라고 했으면 여기서도 name으로 받기)
+    }
+
+
+    // Request Sample
+    // GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
+    // @ModelAttribute를 사용해서 데이터를 받아올 때는
+    // Setter 혹은 오버로딩된 생성자가 꼭 필요함
+    @GetMapping("/form/param/model")
+    @ResponseBody
+    public String helloRequestParam(@ModelAttribute Star star) { // @ModelAttribute는 생략이 가능함
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    /* @ModelAttribute와 @RequestParam 둘다 생략이 가능한데, Spring은 이를 어떻게 구분하는가?
+    * Spring은 해당 파라미터가 SimpleValueType이면 @RequestParam으로 간주,
+    * 아니라면 @ModelAttribute로 간주한다.
+    * SimpleValueType - int(원시타입), Integer(Wrapper), Date 등등
+    * 우리가 만든 클래스가 오브젝트 일때는 ModelAttribute 생략으로 간주 */
+
+    // Request Sample
+    // POST http://localhost:8080/hello/request/form/json
+    // Header
+    // Content type: application/json
+    // Body
+    // {"name":"Robbie", "age":95}
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) { // RequestBody annotation으로 이거 json 형태로 받아주세요~ 라고 알림
+        // 필드를 꼭 맞춰주어야 함
+        return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
 
 
 }
